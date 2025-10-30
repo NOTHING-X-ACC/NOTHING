9d1843a95038c07afdab45bbcfd3da2ac47a9d34
NOTHING-X-ACC <ufprince8@gmail.com>
2025-10-30 10:32:35 +0500

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -16,7 +16,7 @@ async (conn, mek, m, { from, q, reply }) => {
 
         // ❓ If no question given — show guidance
         if (!q) {
-            const guidance = `*APKE PASS KOI SAWAL HAI 🤔 AUR APKO USKA JAWAB NAHI MIL RAHA 🥺* 
+            const guidance = `*APKE PASS KOI SAWAL HAI 🤔 AUR APKO USKA JAWAB NAHI MIL RAHA 🥺 KIA ME APKE SAWAL KA JAWAB DHUND DOO 😇* 
 *TO AP ESE LIKHO ☺️*
 
 *GPT ❮ APKA SAWAL ❯*
