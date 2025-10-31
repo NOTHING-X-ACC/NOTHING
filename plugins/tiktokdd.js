401ddd5d24c2fb709464be64a34a462457428d89
NOTHING-X-ACC <ufprince8@gmail.com>
2025-10-31 17:20:45 +0500

    Create tiktokdd.js

---
+++ plugins/tiktokdd.js
@@ -0,0 +1,40 @@
+const { cmd } = require('../command');
+const axios = require('axios');
+
+cmd({
+pattern: "tiktokdd",
+alias: ["ttdl", "tt", "tiktokdl"],
+desc: "Download TikTok video without watermark",
+category: "downloader",
+react: "🥺",
+filename: __filename
+},
+async (conn, mek, m, { from, args, q, reply }) => {
+try {
+if (!q) return reply("AGAR AP NE TIKTOK KI VIDEO DOWNLOAD KARNI HAI 🥺💓 \n TO AP ESE LIKHO 😇♥️ \n \n TIKTOK ❮TIKTOK VIDEO LINK❯ \n\n AP APNI TIKTOK VIDEO KA LINK COMMAND ❮TIKTOK❯ LIKH KER ☺️ \n USKE AGE APNI TIKTOK VIDEO KA LINK PASTE KAR DO 😊 \n TO APKI TIKTOK VIDEO DOWNLOAD KARNE KE BAAD 😍 \n YAHA BHEJ DE JAYE GE 🥰");
+if (!q.includes("tiktok.com")) return reply("Invalid TikTok link.");
+
+reply("*APKI TIKTOK VIDEO DOWNLOAD HO RAHI HAI...☺️🌹*");  
+      
+    const apiUrl = `https://delirius-apiofc.vercel.app/download/tiktok?url=${q}`;  
+    const { data } = await axios.get(apiUrl);  
+      
+    if (!data.status || !data.data) return reply("*APKI TIKTOK VIDEO NAHI MILI 😔*");  
+      
+    const { title, like, comment, share, author, meta } = data.data;  
+    const videoUrl = meta.media.find(v => v.type === "video").org;  
+      
+    const caption = '*👑 BY :❯ BILAL-MD 👑*';  
+      
+    await conn.sendMessage(from, {  
+        video: { url: videoUrl },  
+        caption: caption,  
+        contextInfo: { mentionedJid: [m.sender] }  
+    }, { quoted: mek });  
+      
+} catch (e) {  
+    console.error("*DUBARA KOSHISH KARE 😔*", e);  
+    reply(`ERROR ${e.message}`);  
+}
+
+});
